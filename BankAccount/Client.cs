ebace164af7a257e863d3abd28eb208d1370c7bf
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-07-14 17:03:58 -0400

    2

---
+++ BankAccount/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class Client
+    {
+        private string hairColor;
+        private string eyeColor;
+        private string name;
+
+        public string HairColor { get; }
+        public string EyeColor { get; }
+        public string Nmae { get; }
+
+        public Client()
+        {
+            //defalt
+        }
+        //need method
+    }
+}
--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -9,7 +9,34 @@ namespace BankAccount
     class Program
     {
         static void Main(string[] args)
-        {
+        {   // do while until exit
+            // menu switch case calls methods
+            Console.WriteLine("\tWeclome to FakeBank, please enter a selection.\n");
+            Console.WriteLine("\t\t[1]We know what you look like.\n\n\t\t[2] View account balences."
+                + "\n\n\t\t[3]Deposit Funds \n\n\t\t[4]Withdraw Funds \n\n\t\t[5]Exit");
+            string input = Console.ReadLine();
+            switch(input)
+            {
+                case "1":
+                    //call client info
+                    break;
+                case "2":
+                    Console.WriteLine("");
+                    break;
+                case "3":
+
+                    break;
+                case "4":
+
+                    break;
+
+                    
+
+
+            }
+            
+            // instantiate client, checking, saveings
+
         }
     }
 }
