54ae973a60348501613a1da2ac373f135ed42af4
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-07-15 13:15:05 -0400

    all classes

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -12,7 +12,7 @@ namespace BankAccount
         {   // do while until exit
             // menu switch case calls methods
             Console.WriteLine("\tWeclome to FakeBank, please enter a selection.\n");
-            Console.WriteLine("\t\t[1]We know what you look like.\n\n\t\t[2] View account balences."
+            Console.WriteLine("\t\t[1]We know what you look like.\n\n\t\t[2]View account balences."
                 + "\n\n\t\t[3]Deposit Funds \n\n\t\t[4]Withdraw Funds \n\n\t\t[5]Exit");
             string input = Console.ReadLine();
             switch(input)
@@ -21,13 +21,19 @@ namespace BankAccount
                     //call client info
                     break;
                 case "2":
-                    Console.WriteLine("");
+                    Console.WriteLine("\t Which balance would you like?" 
+                        + "\n\t\t[A] Checking Account\n\t\t[B] Saveings Account");
+                    string accountSelect = Console.ReadLine();
                     break;
                 case "3":
-
+                    Console.WriteLine("\t For which account would you like make a deposit?"
+                   + "\n\t\t[A] Checking Account\n\t\t[B] Saveings Account");
+                    string depositSelect = Console.ReadLine();
                     break;
                 case "4":
-
+                    Console.WriteLine("\t For which account would you like to make a withdraw?"
+                 + "\n\t\t[A] Checking Account\n\t\t[B] Saveings Account");
+                    string withdrawSelect = Console.ReadLine();
                     break;
 
                     
